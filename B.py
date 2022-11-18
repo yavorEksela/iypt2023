aac3955eabde1d873e332f2800d8fba2aea9e93a
yavorEksela <117588849+yavorEksela@users.noreply.github.com>
2022-11-18 20:18:12 +0200

    Update B.py

--- B.py
+++ B.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import tplquad,IntegrationWarning
+from numba import jit
 
 mu_0=4*np.pi*10**-7
 
@@ -10,27 +11,37 @@ def B(r0,h,R,H,M):
         M=Br/mu_0 - магнетизация на магнита, където Br е остатъчната индукция, 
         която може да се види на сайта на прамиз
         Всичко трябва да бъде в основни мерни единици!'''
+    @jit
     def rho(z,r,phi): # разстоянието до точката
         return np.sqrt((r0-r*np.cos(phi))**2+(r*np.sin(phi))**2+(H+h-z)**2)
 
+    @jit
     def dBx_r(z,r,phi): #диференциал на х-компонентата
         return (3*(H+h-z)*(r0-r*np.cos(phi))/rho(z,r,phi)**5)*r
 
-    def dBy_r(z,r,phi): #диференциал на у-компонентата
-        return (-3*(H+h-z)*r*np.sin(phi)/rho(z,r,phi)**5)*r
+    #@jit
+    #def dBy_r(z,r,phi): #диференциал на у-компонентата
+    #    return (-3*(H+h-z)*r*np.sin(phi)/rho(z,r,phi)**5)*r
 
+    @jit
     def dBz_r(z,r,phi): #диференциал на z-компонентата
-        return (3*(H+h-z)**2/rho(z,r,phi)**5-1/rho(z,r,phi)**3)*r
+        _rho=rho(z,r,phi)
+        return (3*(H+h-z)**2/_rho**5-1/_rho**3)*r
     try:
         Bx=mu_0*M/(4*np.pi)*tplquad(dBx_r,0,2*np.pi,lambda phi: 0,lambda phi: R,lambda phi,r: 0,lambda phi,r: H)[0]
     except IntegrationWarning: #тук не помня защо ми се е налагало да оправям тази грешка
         Bx=0
-    try:
-        By=mu_0*M/(4*np.pi)*tplquad(dBy_r,0,2*np.pi,lambda phi: 0,lambda phi: R,lambda phi,r: 0,lambda phi,r: H)[0]
-    except IntegrationWarning:
-        By=0
+    #try:
+    #    By=mu_0*M/(4*np.pi)*tplquad(dBy_r,0,2*np.pi,lambda phi: 0,lambda phi: R,lambda phi,r: 0,lambda phi,r: H)[0]
+    #except IntegrationWarning:
+    #    By=0
+    By=0
     Bz=mu_0*M/(4*np.pi)*tplquad(dBz_r,0,2*np.pi,lambda phi: 0,lambda phi: R,lambda phi,r: 0,lambda phi,r: H)[0]
     return np.array((Bx,By,Bz))
 
 if __name__=="__main__":
+    import time
+    t1=time.time()
     print(B(0.001,0.003,0.2,0.1,1.26/mu_0)) #тук стойностите са леко произволни
+    t2=time.time()
+    print(t2-t1)
